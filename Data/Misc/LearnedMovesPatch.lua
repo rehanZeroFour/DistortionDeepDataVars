0774242069db03ba3605ea6ffa7cd19df77b1be0
rehanZeroFour <165962506+rehanZeroFour@users.noreply.github.com>
2026-06-08 19:01:50 -0400

    Update learned moves for axewroria, fraxureroria, and haxorusroria

--- Data/Misc/LearnedMovesPatch.lua
+++ Data/Misc/LearnedMovesPatch.lua
@@ -1,7 +1,7 @@
 return {
 	Formes = {
-		axewroria = {levelUp={{1,150}}},  -- Splash only, via external
-		fraxureroria = {levelUp={{1,300}}},  -- Splash only, via external
-		haxorusroria = {levelUp={{1,450}}},  -- Splash only, via external
+		axewroria = {levelUp={{1,150, 'tackle'}}},  -- Splash only, via external
+		fraxureroria = {levelUp={{1,300, 'nightslash'}}},  -- Splash only, via external
+		haxorusroria = {levelUp={{1,450, 'gigaimpact'}}},  -- Splash only, via external
 	},
 }
