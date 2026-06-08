cb6030c8281978cd7edde2dc29f655fefe9507cd
rehanZeroFour <165962506+rehanZeroFour@users.noreply.github.com>
2026-06-08 18:49:00 -0400

    Update LearnedMovesPatch.lua

--- Data/Misc/LearnedMovesPatch.lua
+++ Data/Misc/LearnedMovesPatch.lua
@@ -1,5 +1,7 @@
 return {
 	Formes = {
 		axewroria = {levelUp={{1,150}}},  -- Splash only, via external
+		fraxureroria = {levelUp={{1,300}}},  -- Splash only, via external
+		haxorusroria = {levelUp={{1,450}}},  -- Splash only, via external
 	},
 }
