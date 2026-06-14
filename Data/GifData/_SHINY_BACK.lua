87c5d8b5056b8473418418e0f2df8d9d0e6ba3f5
Craw <crawman.mail@gmail.com>
2026-06-14 20:08:40 +0200

    Update _SHINY_BACK.lua

--- Data/GifData/_SHINY_BACK.lua
+++ Data/GifData/_SHINY_BACK.lua
@@ -121,7 +121,7 @@ return {
 		["Slowbro-Roria"]={sheets={{id=87123214686635,rows=10}},nFrames=47,fWidth=71,fHeight=80,framesPerRow=5},
 		["Mareep-Roria"]={sheets={{id=174994377,startPixelY=258,rows=5},{id=174994393,rows=5},},nFrames=74,fWidth=69,fHeight=56,framesPerRow=8},
 		["Flaaffy-Roria"]={sheets={{id=174989322,startPixelY=341,rows=3},{id=174989338,rows=3},},nFrames=47,fWidth=64,fHeight=65,framesPerRow=9},
-		["Ampharos-Roria"]={sheets={{id=92742819387430,rows=6}},nFrames=47,fWidth=77,fHeight=123,framesPerRow=8,inAir=0.5},
+		["Ampharos-Roria"]={sheets={{id=75014112037006,rows=6}},nFrames=47,fWidth=77,fHeight=123,framesPerRow=8,inAir=0.5},
 		['Mawile-Roria']={sheets={{id=121866159347756,rows=10}},nFrames=48,fWidth=84,fHeight=95,framesPerRow=5,scale=0.65},
 		['Sableye-Roria']={sheets={{id=91485434787839,rows=7}},nFrames=48,fWidth=74,fHeight=102,framesPerRow=7,scale=0.65},
 		['Treecko-Roria']={sheets={{id=87522122076041,rows=7}},nFrames=48,fWidth=90,fHeight=87,framesPerRow=7},
