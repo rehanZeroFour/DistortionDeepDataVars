5b6f195f22b2e86e20522d42a9459e04bf83667b
rehanZeroFour <165962506+rehanZeroFour@users.noreply.github.com>
2026-07-28 05:02:08 -0400

    Update ChunkData.lua

--- Data/Misc/ChunkData.lua
+++ Data/Misc/ChunkData.lua
@@ -3,35 +3,29 @@ return function(env)
 	local OldRodList = env.OldRodList
 	local GoodRodList = env.GoodRodList
 	local SuperRodList = env.SuperRodList
+	local Variant = env.Variant
 
 	return {
 		['chunk1'] = {
 			regions = {
-				['Mitis Town'] = {
-					OldRod = OldRodList {
-						{'Magikarp', 80},
-						{'Arrokuda', 20},
-					},
-					GoodRod = GoodRodList {
-						{'Magikarp', 80},
-						{'Gyarados', 5},
-					},
-				},
 				['Route 1'] = {
-					Music = 14320023424,
-					Grass = EncounterList {
-						{'Nickit',     2, 4, 25},
-						{'Zigzagoon',  2, 4, 25, 'day'},
-						{'Zigzagoon',  2, 4, 25, 'night', false, 'Galar'},
-						{'Pidgey',     2, 4, 25},
-						{'Bunnelby',   2, 4, 25},
-						{'Wurmple',    2, 4, 25},
-						{'Sentret',    2, 4,  5, 'day'},
-						{'Fletchling', 2, 4, 11},
-						{'Pidgey',     2, 4, 4},
-					} 'Hard' {
-						{'Giratina',     4, 6, 25},
-					},
+					Grass = Variant(
+						EncounterList {
+							{'Nickit',     2, 4, 25},
+							{'Zigzagoon',  2, 4, 25, 'day'},
+							{'Zigzagoon',  2, 4, 25, 'night', false, 'Galar'},
+							{'Pidgey',     2, 4, 25},
+							{'Bunnelby',   2, 4, 25},
+							{'Wurmple',    2, 4, 25},
+							{'Sentret',    2, 4,  5, 'day'},
+							{'Fletchling', 2, 4, 11},
+							{'Pidgey',     2, 4, 4},
+						},
+						'Hard',
+						{
+							{'Giratina',     4, 6, 25},
+						}
+					),
 				},
 			},
 		},
