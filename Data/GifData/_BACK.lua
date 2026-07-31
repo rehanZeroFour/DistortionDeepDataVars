ac6241d7f737250502d2f02b0d0dd5407648d73e
Craw <crawman.mail@gmail.com>
2026-07-31 23:03:59 +0200

    Update _BACK.lua

--- Data/GifData/_BACK.lua
+++ Data/GifData/_BACK.lua
@@ -147,7 +147,7 @@ return {
 		["Nidoking-Crawlympics"]={sheets={{id=108863249784633,rows=8}},nFrames=46,fWidth=136,fHeight=102,framesPerRow=6,speed=0.05,scale=0.8},
 		["Nidoqueen-Crawlympics"]={sheets={{id=133485728048146,rows=8}},nFrames=46,fWidth=90,fHeight=106,framesPerRow=6,speed=0.05,scale=0.7},
 		['Houndoom-Crawlympics']={sheets={{id=121677477986272,rows=6}},nFrames=48,fWidth=79,fHeight=67,framesPerRow=8},
-		['Manectric-Crawlympics']={sheets={{id=119120228410798,rows=6}},nFrames=48,fWidth=46,fHeight=76,framesPerRow=8,scale=0.8},	
+		['Manectric-Crawlympics']={sheets={{id=119120228410798,rows=6}},nFrames=48,fWidth=46,fHeight=76,framesPerRow=8,scale=1.1},	
 	
 		--Hard Mode Mons
 		['Archopteryx']={sheets={{id=74437527423603,rows=8}},nFrames=40,fWidth=190,fHeight=113,framesPerRow=5},
@@ -174,7 +174,7 @@ return {
 		["Flaaffy-Roria"]={sheets={{id=174989322,startPixelY=341,rows=3},{id=174989338,rows=3},},nFrames=47,fWidth=64,fHeight=65,framesPerRow=9},
 		["Ampharos-Roria"]={sheets={{id=92742819387430,rows=6}},nFrames=47,fWidth=77,fHeight=123,framesPerRow=8,inAir=0.5,scale=.8},
 		['Houndoom-Roria']={sheets={{id=121677477986272,rows=6}},nFrames=48,fWidth=79,fHeight=67,framesPerRow=8},
-		['Manectric-Roria']={sheets={{id=119120228410798,rows=6}},nFrames=48,fWidth=46,fHeight=76,framesPerRow=8,scale=0.8},	
+		['Manectric-Roria']={sheets={{id=119120228410798,rows=6}},nFrames=48,fWidth=46,fHeight=76,framesPerRow=8,scale=1.1},	
 		['Mawile-Roria']={sheets={{id=106914645080953,rows=10}},nFrames=48,fWidth=84,fHeight=95,framesPerRow=5,scale=0.65},
 		['Sableye-Roria']={sheets={{id=71734385252449,rows=7}},nFrames=48,fWidth=74,fHeight=102,framesPerRow=7,scale=0.65},
 		['Treecko-Roria']={sheets={{id=87522122076041,rows=7}},nFrames=48,fWidth=90,fHeight=87,framesPerRow=7},
